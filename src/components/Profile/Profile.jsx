7cc6dfd886d6e925772a087655c54420957d2045
1Rendes <1admin@ukr.net>
2024-07-19 20:59:12 +0200

    fix task 1 img

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,38 +1,38 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-irregular-whitespace */
-import css from './Profile.module.css'
-import clsx from 'clsx'
+import css from "./Profile.module.css";
+import clsx from "clsx";
 
 const Profile = (props) => {
-    return (
+  return (
     <div className={clsx(css.profile)}>
- <div className={clsx(css.profileBlock)}
->
-                <img className={clsx(css.profilePhoto)}
-     src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
-     alt="User avatar"/>
+      <div className={clsx(css.profileBlock)}>
+        <img
+          className={clsx(css.profilePhoto)}
+          src={props.image}
+          alt="User avatar"
+        />
         <p className={clsx(css.mainSign)}>{props.name}</p>
         <p className={clsx(css.secondarySign)}>@{props.tag}</p>
         <p className={clsx(css.secondarySign)}>{props.location}</p>
- </div>
+      </div>
 
- <ul className={clsx(css.listStyle)}>
- <li className={clsx(css.elStyle)}>
- <span className={clsx(css.elName)}>Followers</span>
- <span className={clsx(css.elValue)}>{props.stats.followers}</span>
- </li>
- <li className={clsx(css.elStyle, css.elDivider)}>
- <span className={clsx(css.elName)}>Views</span>
- <span className={clsx(css.elValue)}>{props.stats.views}</span>
- </li>
- <li className={clsx(css.elStyle)}>
- <span className={clsx(css.elName)}>Likes</span>
-        <span className={clsx(css.elValue)}>{props.stats.likes}</span>
- </li>
- </ul>
-</div>
-
-)
-}
+      <ul className={clsx(css.listStyle)}>
+        <li className={clsx(css.elStyle)}>
+          <span className={clsx(css.elName)}>Followers</span>
+          <span className={clsx(css.elValue)}>{props.stats.followers}</span>
+        </li>
+        <li className={clsx(css.elStyle, css.elDivider)}>
+          <span className={clsx(css.elName)}>Views</span>
+          <span className={clsx(css.elValue)}>{props.stats.views}</span>
+        </li>
+        <li className={clsx(css.elStyle)}>
+          <span className={clsx(css.elName)}>Likes</span>
+          <span className={clsx(css.elValue)}>{props.stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
 export default Profile;
