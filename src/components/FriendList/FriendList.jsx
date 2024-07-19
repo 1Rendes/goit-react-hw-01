f71ce58e37f0691fc3931b08cc234b738d5992ae
1Rendes <1admin@ukr.net>
2024-07-19 09:40:14 +0200

    friendlistitem location fix

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,15 +1,21 @@
 /* eslint-disable react/prop-types */
-import FriendListItem from "./FriendListItem"
-import css from './FriendList.module.css'
-import clsx from 'clsx'
+import FriendListItem from "../FriendListItem/FriendListItem";
+import css from "./FriendList.module.css";
+import clsx from "clsx";
 
-const FriendList = ({friends}) => {
-    return (
-        <ul className={clsx(css.friendList)}>{friends.map(({id, avatar, name, isOnline}) => (
-            <FriendListItem key={id} avatar={avatar} name={name} isOnline={isOnline} />
-            ))}
-        </ul>
-    )
-}
+const FriendList = ({ friends }) => {
+  return (
+    <ul className={clsx(css.friendList)}>
+      {friends.map(({ id, avatar, name, isOnline }) => (
+        <FriendListItem
+          key={id}
+          avatar={avatar}
+          name={name}
+          isOnline={isOnline}
+        />
+      ))}
+    </ul>
+  );
+};
 
-export default FriendList
+export default FriendList;
